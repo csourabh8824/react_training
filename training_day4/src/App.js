e1ff442a161909f1abb9dfeeca96944f370416b8
csourabh8824 <csourabh8824@gmail.com>
2021-07-22 13:30:02 +0530

    Included form tag in app file

--- training_day4/src/App.js
+++ training_day4/src/App.js
@@ -56,29 +56,30 @@ class App extends React.Component{
     return (
       <div className="App">
         <h1>Form Validation</h1>
-        Name: <input type="text" placeholder="Enter Name" onChange={(e) => { this.setState({ name: e.target.value }) }} /><p style={{color:"red"}}>{ this.state.nameError }</p><br/>
-        Email: <input type="email" placeholder="Enter Email" onChange={(e) => { this.setState({ email: e.target.value }) }} /><p style={{color:"red"}}>{ this.state.emailError }</p><br/><br />
-        Dob: <input style={{ marginRight: "95px" }} type="date" placeholder="Enter date" onChange={(e) => { this.setState({ dob: e.target.value }) }} /><br/><br />
-        <label for="cars">Choose an education level:</label>
-        <select name="education" id="education">
-          <option value="10th">10th</option>
-          <option value="12th">12th</option>
-          <option value="B.E">B.E</option>
-      
-        </select><br /><br />
-        Password: <input type="password" placeholder="Enter Password" onChange={(e) => { this.setState({ password: e.target.value }) }} /><p style={{color:"red"}}>{ this.state.passwordError }</p><br /><br />
-        Confirm Password: <input type="password" placeholder="Enter Password" onChange={(e) => { this.setState({ confirmPassword: e.target.value }) }} /><p style={{color:"red"}}>{ this.state.confirmPasswordError }</p><br/><br />
-        <p style={{ marginRight: "70px" }} >Gender:
-          <input  type="radio" id="html" name="gender" value="Male" onChange={(e) => { this.setState({ gender: e.target.value }) }} />
-          <label for="male">Male</label>
-          <input type="radio" id="css" name="gender" value="Female" onChange={(e) => { this.setState({ gender: e.target.value }) }} />
-          <label for="female">Female</label><br></br> 
-        </p>
-        <p style={{ marginRight: "-55px" }} >Profile Pic: 
-          <input  type="file" onChange={(e) => { this.setState({ profilePic: e.target.value }) }}/>
-        </p><br />
-        <button onClick={()=>{this.submit()}} className="btn btn-primary">Submit</button>
-
+        <form onSubmit={(e)=>e.preventDefault()}>
+          Name: <input type="text" placeholder="Enter Name" onChange={(e) => { this.setState({ name: e.target.value }) }} /><p style={{color:"red"}}>{ this.state.nameError }</p><br/>
+          Email: <input type="email" placeholder="Enter Email" onChange={(e) => { this.setState({ email: e.target.value }) }} /><p style={{color:"red"}}>{ this.state.emailError }</p><br/><br />
+          Dob: <input style={{ marginRight: "95px" }} type="date" placeholder="Enter date" onChange={(e) => { this.setState({ dob: e.target.value }) }} /><br/><br />
+          <label for="cars">Choose an education level:</label>
+          <select name="education" id="education">
+            <option value="10th">10th</option>
+            <option value="12th">12th</option>
+            <option value="B.E">B.E</option>
+        
+          </select><br /><br />
+          Password: <input type="password" placeholder="Enter Password" onChange={(e) => { this.setState({ password: e.target.value }) }} /><p style={{color:"red"}}>{ this.state.passwordError }</p><br /><br />
+          Confirm Password: <input type="password" placeholder="Enter Password" onChange={(e) => { this.setState({ confirmPassword: e.target.value }) }} /><p style={{color:"red"}}>{ this.state.confirmPasswordError }</p><br/><br />
+          <p style={{ marginRight: "70px" }} >Gender:
+            <input  type="radio" id="html" name="gender" value="Male" onChange={(e) => { this.setState({ gender: e.target.value }) }} />
+            <label for="male">Male</label>
+            <input type="radio" id="css" name="gender" value="Female" onChange={(e) => { this.setState({ gender: e.target.value }) }} />
+            <label for="female">Female</label><br></br> 
+          </p>
+          <p style={{ marginRight: "-55px" }} >Profile Pic: 
+            <input  type="file" onChange={(e) => { this.setState({ profilePic: e.target.value }) }}/>
+          </p><br />
+          <button onClick={()=>{this.submit()}} className="btn btn-primary">Submit</button>
+        </form>
       </div>
     );
   }
